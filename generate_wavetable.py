d93c3e0883711468c2ffeb2ac6355b7eafdc4ef3
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-03-10 22:06:01 +0200

    Added python code for generating sine wavatables.

--- generate_wavetable.py
+++ generate_wavetable.py
@@ -3,7 +3,7 @@
 import argparse
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def sine(sample_count, bits):
@@ -13,6 +13,7 @@ def sine(sample_count, bits):
 
     :param sample_count: Number of samples
     :param bits: Bit count per sample
+    :rtype: Numpy array of unsigned integers
     """
     step = 2 * math.pi / sample_count
     amplitude = 2**(bits-1)/2
@@ -20,20 +21,41 @@ def sine(sample_count, bits):
     gen_sample = lambda i: amplitude * np.sin(step*i) + amplitude
     return np.array([gen_sample(i) for i in range(sample_count)], dtype=np.uint) 
 
+def to_c_hex_array(wavetable):
+    """
+    Converts a wavetable buffer to a C char array with hex values.
+
+    :param wavetable: Numpy array of unsigned int values
+    :rtype: List of hex strings inf 0x00 format
+    """
+    return ["{0:#0{1}x}".format(s,4) for s in wavetable]
+
+def format_as_variable(values):
+    """
+    Creates a string with C array definition with the values
+    given as parameter.
+    """
+    str_values = ", ".join(values)
+    return f"const char wavetable[] = {{ { str_values } }};\n"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("waveform", type=str)
     parser.add_argument("bits", type=int)
     parser.add_argument("sampling_frequency", type=int)
-    parser.add_argument("--enable_bandlimiting", type=bool)
-    parser.add_argument("--filter_frequency", type=int)
+    parser.add_argument("--output", type=str)
     args = parser.parse_args()
 
     if args.waveform == "sine":
         wavetable = sine(args.sampling_frequency, args.bits)
-        for s in np.nditer(wavetable):
-            print('{}'.format(s))
-        # print(["{}".format(np.base_repr(s, base=16)) for s in wavetable])
-        plt.plot(wavetable)
-        plt.show()
+        hex_values = to_c_hex_array(wavetable)
+        result = format_as_variable(hex_values)
+    else:
+        raise argparse.ArgumentError("Waveform must be sine")
+
+    # If no output define print into console
+    if not args.output:
+        print(result)
+    else:
+        with open(args.output, "w") as f:
+            f.write(result)
