e688cbfe4ddff224287f51f0203b757e876cc5f8
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-03-13 22:30:29 +0200

    Added ramp up and ramp down waveforms to generator script.

--- generate_wavetable.py
+++ generate_wavetable.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+# coding: future_fstrings 
+
 
 import argparse
 import math
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 """
 A script for generating C array wavetables for the DLFO module.
@@ -20,7 +22,7 @@ def sine(sample_count, bits):
     :rtype: Numpy array of unsigned integers
     """
     step = 2 * math.pi / sample_count
-    amplitude = 2**(bits-1)/2
+    amplitude = 2**bits / 2
 
     gen_sample = lambda i: amplitude * np.sin(step*i) + amplitude
     return np.array([gen_sample(i) for i in range(sample_count)], dtype=np.uint) 
@@ -36,10 +38,10 @@ def triangle(sample_count, bits):
     """
     step = 2 * math.pi / sample_count
     midpoint = int(sample_count / 2)
-    amplitude = 2**(bits-1) / 2
+    amplitude = 2**bits
 
-    gen_sample_falling = lambda i: (4 * amplitude) - ((2 * amplitude) / math.pi) * (i * step)
-    gen_sample_rising = lambda i: ((2 * amplitude) / math.pi) * (i * step)
+    gen_sample_rising = lambda i: (amplitude / math.pi) * (i * step)
+    gen_sample_falling = lambda i: 2 * amplitude - (amplitude / math.pi) * (i * step)
 
     rising = np.array([gen_sample_rising(i) for i in range(midpoint)], dtype=np.uint) 
     falling = np.array([gen_sample_falling(i) for i in range(midpoint, sample_count)], dtype=np.uint) 
@@ -55,19 +57,44 @@ def square(sample_count, bits):
     :param bits: Bit count per sample
     :rtype: Numpy array of unsigned integers
     """
-    step = 2 * math.pi / sample_count
     midpoint = int(sample_count / 2)
-    amplitude = 2**(bits-1) / 2
+    amplitude = 2**bits
 
-    gen_sample = lambda i: amplitude * 2 if i < midpoint else 0
+    gen_sample = lambda i: amplitude if i < midpoint else 0
 
     return np.array([gen_sample(i) for i in range(sample_count)], dtype=np.uint) 
 
-def ramp_up(sample_count, bits):
-    pass
+def rampup(sample_count, bits):
+    """
+    Generate a *sample_count* size array containing a rampup wave cycle.
+    Amplitude is defined by the desired bit count.
 
-def ramp_down(sample_count, bits):
-    pass
+    :param sample_count: Number of samples
+    :param bits: Bit count per sample
+    :rtype: Numpy array of unsigned integers
+    """
+    step = 2 * math.pi / sample_count
+    amplitude = 2**bits
+
+    gen_sample = lambda i: (amplitude / 2 * math.pi) * (step * i)
+
+    return np.array([gen_sample(i) for i in range(sample_count)], dtype=np.uint) 
+
+def rampdown(sample_count, bits):
+    """
+    Generate a *sample_count* size array containing a rampdown wave cycle.
+    Amplitude is defined by the desired bit count.
+
+    :param sample_count: Number of samples
+    :param bits: Bit count per sample
+    :rtype: Numpy array of unsigned integers
+    """
+    step = 2 * math.pi / sample_count
+    amplitude = 2**bits
+
+    gen_sample = lambda i: 2 * amplitude - ((amplitude / 2 * math.pi) * (step * i))
+
+    return np.array([gen_sample(i) for i in range(sample_count)], dtype=np.uint)
 
 
 def to_c_hex_array(wavetable):
@@ -95,28 +122,20 @@ if __name__ == '__main__':
     parser.add_argument("--output", type=str)
     args = parser.parse_args()
 
-    if args.waveform == "sine":
-        wavetable = sine(args.sampling_frequency, args.bits)
+    wave_functions = ["sine", "triangle", "square", "rampup", "rampdown"]
+    if args.waveform in wave_functions:
+        wave_func = locals()[args.waveform]
+        wavetable = wave_func(args.sampling_frequency, args.bits)
         hex_values = to_c_hex_array(wavetable)
         result = format_as_variable(hex_values)
-    elif args.waveform == "triangle":
-        wavetable = triangle(args.sampling_frequency, args.bits)
-        hex_values = to_c_hex_array(wavetable)
-        result = format_as_variable(hex_values)
-        # plt.plot(wavetable)
-        # plt.show()
-    elif args.waveform == "square":
-        wavetable = square(args.sampling_frequency, args.bits)
-        hex_values = to_c_hex_array(wavetable)
-        result = format_as_variable(hex_values)
-        # plt.plot(wavetable)
-        # plt.show()
+        plt.plot(wavetable)
+        plt.show()
     else:
         raise argparse.ArgumentError("Waveform must be sine")
 
     # If no output define print into console
-    # if not args.output:
-    #     print(result)
-    # else:
-    #     with open(args.output, "w") as f:
-    #         f.write(result)
+    if not args.output:
+        print(result)
+    else:
+        with open(args.output, "w") as f:
+            f.write(result)
